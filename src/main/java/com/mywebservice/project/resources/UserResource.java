3e40f3bf3b1d56417adf377961ebda1c39e89e6b
luisaglicio <luisaglicio@gmail.com>
2021-06-15 22:14:57 -0300

    Initial test, OK!

---
+++ src/main/java/com/mywebservice/project/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.mywebservice.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mywebservice.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Luis", "luisaglicio@gmail.com", "991742838", "12345");
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
