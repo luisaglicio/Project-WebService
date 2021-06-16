89cbd9ac9c714151e171e549f36758eb4d44c89b
luisaglicio <luisaglicio@gmail.com>
2021-06-15 22:57:53 -0300

    configure h2 database for tests, Ok!

--- src/main/java/com/mywebservice/project/entities/User.java
+++ src/main/java/com/mywebservice/project/entities/User.java
@@ -2,10 +2,23 @@ package com.mywebservice.project.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//anotação para instruir meu JPA como ele deve converter os objetos para o modelo relascional
+
+@Entity
 public class User implements Serializable{
 		
 	private static final long serialVersionUID = 1L;
 	
+	//anotação para informar para banco que a primary Key será o ID
+	//anotação para meu auto increment do banco
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
