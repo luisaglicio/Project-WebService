7381132bb9791b33af765238ce95b6e92cf37d2c
luisaglicio <luisaglicio@gmail.com>
2021-06-17 00:02:38 -0300

    create package service, test with postman method get, Ok!

--- src/main/java/com/mywebservice/project/resources/UserResource.java
+++ src/main/java/com/mywebservice/project/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.mywebservice.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mywebservice.project.entities.User;
+import com.mywebservice.project.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Luis", "luisaglicio@gmail.com", "991742838", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 		
+	}
+	
+	@GetMapping(value = "/{id}")
+	//feita a anotação, para o spring entender que o meu findById precisa puxar este valor "/{id}"
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
