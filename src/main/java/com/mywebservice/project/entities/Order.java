b2bab9f2ead0842bdaf94d64b57931932d04e091
luisaglicio <luisaglicio@gmail.com>
2021-06-21 20:48:27 -0300

    create order Status, test Ok!

--- src/main/java/com/mywebservice/project/entities/Order.java
+++ src/main/java/com/mywebservice/project/entities/Order.java
@@ -12,37 +12,39 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
-
-
+import com.mywebservice.project.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	//Anotação feita para formatar o Json
+
+	// Anotação feita para formatar o Json
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	//anotação para informar que serão muitos para 1 (muitos pedidos para um cliente)
-	//Anotação JoinColumn para a chave estrangeira
+
+	private Integer orderStatus;
+
+	// anotação para informar que serão muitos para 1 (muitos pedidos para um
+	// cliente)
+	// Anotação JoinColumn para a chave estrangeira
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -60,6 +62,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
@@ -94,7 +106,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
