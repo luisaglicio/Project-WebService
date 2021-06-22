c89159c837cb2e608f198b7bc816a249fcd307c4
luisaglicio <luisaglicio@gmail.com>
2021-06-21 21:55:31 -0300

    create class product, test Ok!

--- src/main/java/com/mywebservice/project/config/TestConfig.java
+++ src/main/java/com/mywebservice/project/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.mywebservice.project.entities.Category;
 import com.mywebservice.project.entities.Order;
+import com.mywebservice.project.entities.Product;
 import com.mywebservice.project.entities.User;
 import com.mywebservice.project.entities.enums.OrderStatus;
 import com.mywebservice.project.repositories.CategoryRepository;
 import com.mywebservice.project.repositories.OrderRepository;
+import com.mywebservice.project.repositories.ProductRepository;
 import com.mywebservice.project.repositories.UserRepository;
 
 //anotação criada para informar ao spring que esta é uma classe de configuração
@@ -31,7 +33,10 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 
 	@Autowired
-	private CategoryRepository CategoryRepository;
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,7 +46,14 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
-		CategoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		//usuários para test inicial
 		User u1 = new User(null, "Logan", "logan@gmail.com", "988888888", "123456");
---
+++ src/main/java/com/mywebservice/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.mywebservice.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mywebservice.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
