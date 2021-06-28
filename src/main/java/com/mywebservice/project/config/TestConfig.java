24ac71c90574cb69a6e6fc6ece1f1631d3ae8dc1
luisaglicio <luisaglicio@gmail.com>
2021-06-28 19:08:26 -0300

    create class Payment and implement one to one with the class order, test Ok!

--- src/main/java/com/mywebservice/project/config/TestConfig.java
+++ src/main/java/com/mywebservice/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mywebservice.project.entities.Category;
 import com.mywebservice.project.entities.Order;
 import com.mywebservice.project.entities.OrderItem;
+import com.mywebservice.project.entities.Payment;
 import com.mywebservice.project.entities.Product;
 import com.mywebservice.project.entities.User;
 import com.mywebservice.project.entities.enums.OrderStatus;
@@ -89,6 +90,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2021-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
