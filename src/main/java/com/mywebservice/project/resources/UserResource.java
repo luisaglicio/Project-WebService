3a6cef7feb558206dd64216c0e1edfe430f50243
luisaglicio <luisaglicio@gmail.com>
2021-06-28 20:32:46 -0300

    implement method created and delete classes UserService and UserResource, test with postman Ok!

--- src/main/java/com/mywebservice/project/resources/UserResource.java
+++ src/main/java/com/mywebservice/project/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.mywebservice.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mywebservice.project.entities.User;
 import com.mywebservice.project.services.UserService;
@@ -31,6 +36,22 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		//linha criada para no postman dar o retorno 201 e não 200
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	//Anotação para poser usar o methodo post para deletar
+	@DeleteMapping(value = "/{id}")
+	//Anotação para o long Ied ser recoinhecido como uma variavel
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
